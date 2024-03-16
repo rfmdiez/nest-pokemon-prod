a71256f6808b836ce029933fa755ffb61875ac99
ricardomdiez <rfmdiez@gmail.com>
2024-03-15 20:49:48 -0500

    preparar mongodb y envs

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+
+@Injectable()
+export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
+
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=100',
+    );
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      console.log(segments);
+      const no: number = +segments[segments.length - 2];
+
+      //const pokemon = await this.pokemonModel.create({ name, no });
+
+      pokemonToInsert.push({ name, no });
+    });
+
+    this.pokemonModel.insertMany(pokemonToInsert);
+    return 'Seed executed';
+  }
+
+  // async executeSeed() {
+  //   await this.pokemonModel.deleteMany({});
+
+  //   const { data } = await this.axios.get<PokeResponse>(
+  //     'https://pokeapi.co/api/v2/pokemon?limit=10',
+  //   );
+
+  //   const insertPromisesArray = [];
+
+  //   data.results.forEach(({ name, url }) => {
+  //     const segments = url.split('/');
+  //     console.log(segments);
+  //     const no: number = +segments[segments.length - 2];
+
+  //     //const pokemon = await this.pokemonModel.create({ name, no });
+
+  //     insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+  //   });
+
+  //   await Promise.all(insertPromisesArray);
+  //   return 'Seed executed';
+  // }
+}
